125464689f2e0ebaacdc2d0bb169579b4e06f245
Sketch-Turner <awbw.damage.calculator.plus@gmail.com>
2024-07-06 13:25:22 -0500

    Update name_grabber.py

--- name_grabber.py
+++ name_grabber.py
@@ -2,9 +2,8 @@ import gzip
 import os
 import re
 import requests 
-from typing import Optional
 
-
+# extract all previous logs from zip to regular file AND latest file and read file 
 def extractFiles(directory):
     for dirpath, dirnames, filenames in os.walk(directory):
         # print(dirpath, dirnames, filenames)
@@ -12,7 +11,7 @@ def extractFiles(directory):
             filePath = os.path.join(dirpath,file)
             # print(filePath)
             if file.endswith(".gz"):
-                with gzip.open(filePath, "rt",encoding="latin-1") as textFile:
+                with gzip.open(filePath, "rt", encoding="latin-1") as textFile:
                     text = textFile.read()
                     #print(text)
                     getNames(text)
@@ -21,7 +20,7 @@ def extractFiles(directory):
                 #print(text)
                 getNames(text)
                  
-
+# search for unique pattern (FINAL KILL!)
 def getNames(text):
     pattern = r"\[CHAT\] (.+?) was (.+?) FINAL KILL!"
     matches = re.findall(pattern,text)
@@ -30,16 +29,17 @@ def getNames(text):
         UUID = getUUID(username)
         print(UUID,username)
 
-
-def getUUID(username: str) -> Optional[str]:
+# query minecraft api for uuid
+def getUUID(username):
     try:
         return requests.get(f"https://api.mojang.com/users/profiles/minecraft/{username}?").json()["id"]
     except Exception as e:
         return None
     
+
+
 # remove duplicates
 # fix uuid not found
-# extract all previous logs from zip to regular file AND latest file and read file  
+ 
 directoryPath = "C:\\Users\\49151\\Desktop\\Old logs"
 extractFiles(directoryPath)
-  # search for unique pattern (FINAL KILL!)
